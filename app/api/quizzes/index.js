83584c0e094db495e5bc2d7981a52c27ac627f37
LanouxNicolas <nicolas.lanoux@etu.univ-cotedazur.fr>
2020-03-17 09:25:43 +0100

    TP

--- app/api/quizzes/index.js
+++ app/api/quizzes/index.js
@@ -1,12 +1,26 @@
 const { Router } = require('express')
 
 const { Quiz } = require('../../models')
+const QuestionsRouter = require('./questions')
+const { Question } = require('../../models')
 
 const router = new Router()
 
 router.get('/', (req, res) => {
   try {
     res.status(200).json(Quiz.get())
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+router.get('/:quizId', (req, res) => {
+  try {
+    const quiz = Quiz.getById(req.params.quizId)
+    var questions = Question.get().filter(question=>question.quizId==req.params.quizId)
+    quiz.questions=questions   
+    res.status(200).json(quiz)
+
   } catch (err) {
     res.status(500).json(err)
   }
@@ -25,4 +39,23 @@ router.post('/', (req, res) => {
   }
 })
 
+router.delete('/:quizId', (req, res) => {
+  try {
+    res.status(200).json(Quiz.delete(req.params.quizId))
+
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+router.put('/:quizId', (req, res) => {
+  try {
+    res.status(200).json(Quiz.update(req.params.quizId,req.body))
+
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+router.use('/:quizId/questions', QuestionsRouter)
 module.exports = router
